cb4baf5ed937906e7e7ae283381efbf2f4adf096
Brendan Abbott <brendan@bloodbone.ws>
2013-07-07 14:34:42 +1000

    Allow getDimensions to be async. Fixes #13

--- assets/image_index_preview.publish.js
+++ assets/image_index_preview.publish.js
@@ -6,72 +6,57 @@ jQuery(document).ready(function($) {
 		* @author: Symphony Community
 		* @source: https://github.com/symphonists/image_index_preview
 		*/
-
 	 var root, page, link, path, file, size;
-
 	 var defaultSize = 140;
 
 	 root = Symphony.Context.get('root');
 	 page = Symphony.Context.get('env')['page'];
 
-	 function getDimensions(src) {
-			img = document.createElement('img');
-			img.src = src;
+	 function getDimensions(src, link) {
+		var ratio, w, h;
+		img = document.createElement('img');
+		img.src = src;
 
-			img.onload = function() {
-				 return { width: this.width, height: this.height };
-			};
-
-			var ratio;
-			var w = img.onload().width;
-			var h = img.onload().height;
-
+		img.onload = function() {
+			w = this.width;
+			h = this.height;
+			
 			if (h > w) {
 				 ratio = w / h;
 				 size = parseInt(defaultSize * ratio) + '/' + 0;
-			} else {
+			}
+			else {
 				 ratio = h / w;
 				 size = 0 + '/' + parseInt(defaultSize * ratio);
 			}
 
-			return { s: size, r: ratio, h: h, w: w };
+			// add preview
+			$('<img />', {
+				src: root + '/image/1/' + size + '/' + file
+			}).prependTo(link);
+		}
 	 }
 
 	 $('table td[class*="upload"] a, fieldset div[class*="upload"] a', '#contents').each(function() {
+		link = $(this);
 
-			link = $(this);
+		if (page == 'index') {
+			 path = link.data('path');
+			 filename = link.text();
+			 file = path.replace(root, '').replace('/workspace/','') + '/' + filename;
+			 full_filepath = root + path + '/' + filename;
+		}
+		else {
+			 path = link.attr('href');
+			 file = path.replace(root, '').replace('/workspace/','');
+			 full_filepath = path;
+		}
 
-			if (page == 'index') {
-
-				 path = link.data('path');
-				 filename = link.text();
-				 file = path.replace(root, '').replace('/workspace/','') + '/' + filename;
-				 attr = getDimensions(path + '/' + filename);
-
-			} else {
-
-				 path = link.attr('href');
-				 file = path.replace(root, '').replace('/workspace/','');
-
-				 attr = getDimensions(path);
-			}
-
-			if (path) {
-
-				 if (file.match(/\.(?:bmp|gif|jpe?g|png)$/i)) {
-
-						// remove file name
-
-						link.text('');
-
-						// add preview
-
-						$('<img />', {
-
-							 src: root + '/image/1/' + attr.s + '/' + file
-
-						}).prependTo(link);
-				 }
-			}
+		if (path && file.match(/\.(?:bmp|gif|jpe?g|png)$/i)) {
+			// remove file name
+			link.text('');
+			// add image
+			getDimensions(full_filepath, link);
+		}
 	 });
 });
